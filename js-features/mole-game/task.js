b97dfb6e005f839815fb917c88595d2637326d8e
Антон <fisher5235@yandex.ru>
2023-07-28 00:42:17 +0300

    1

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,26 @@
+
+     const dead = document.getElementById("dead");
+     const lost = document.getElementById("lost");
+    
+    for(let i = 1; i < 10; i++) {
+        const getHole = document.getElementById(`hole${i}`);
+        getHole.onclick = () => {
+            if(getHole.className.includes( 'hole_has-mole' )) {
+                dead.textContent++;
+            }else{
+                lost.textContent++;
+            }
+        
+    
+    if(dead.textContent > 5) {
+        alert('You Win!!!');
+        dead.textContent = 0;
+        lost.textContent = 0;
+    } else if(lost.textContent > 5) {
+        alert('You Loose!!!');
+        dead.textContent = 0;
+        lost.textContent = 0;
+    }
+}
+}
+     
