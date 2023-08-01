d1a81b85f0d18f22f072b3120dde597c7861a648
Антон <fisher5235@yandex.ru>
2023-08-02 01:24:26 +0300

    1

---
+++ element-search/popups/task.js
@@ -0,0 +1,9 @@
+const main = document.getElementById('modal_main');
+const close = document.getElementsByClassName('modal__close')
+const success = document.getElementById('modal_success');
+
+main.classList.add('modal_active');
+
+
+
+
