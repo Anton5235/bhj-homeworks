d29015e09bbb63ff1802e268914e321d90ee1df5
Антон <fisher5235@yandex.ru>
2023-08-21 19:40:42 +0300

    1

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -2,16 +2,20 @@ const dropValue = document.querySelector('.dropdown__value');
 const dropList = document.querySelector('.dropdown__list');
 const dropItem = dropList.querySelectorAll('.dropdown__item');
 
+const findItem =  dropItem.forEach(item => 
+    item.addEventListener('click', (event) => {
+       event.preventDefault()
+       dropValue.textContent = item.textContent
+       dropList.classList.remove('dropdown__list_active')
+   })
+)
 
-dropValue.onclick = function () {
+dropValue.addEventListener('click', () => {
+    dropList.classList.toggle('dropdown__list_active')
+    findItem()
+   })
 
-    dropList.classList.toggle ('dropdown__list_active')
 
-    Array.from(dropItem).forEach(item => 
-        item.addEventListener('click', (event) => {
-        event.preventDefault();
-        dropValue.textContent = item.textContent;
-        dropList.classList.remove('dropdown__list_active')
-    }))
     
-};
+    
+
